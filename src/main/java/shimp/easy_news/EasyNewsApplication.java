918a63fc2907e05c615ff451b7bf9886c7e44806
twkwon0417 <kwontony123@konkuk.ac.kr>
2025-05-15 18:40:53 +0900

    first commit

---
+++ src/main/java/shimp/easy_news/EasyNewsApplication.java
@@ -0,0 +1,13 @@
+package shimp.easy_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shimp/easy_news/EasyNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package shimp.easy_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
